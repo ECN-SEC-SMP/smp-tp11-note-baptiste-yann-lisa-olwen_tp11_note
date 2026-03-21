edc79d0792da925688eb1fc09d14e105f5e87c01
Olwen <nonopich@proton.me>
2026-03-21 14:40:54 +0100

    Ajout d'une fonction pour convertir en string

--- CouleurCarte.h
+++ CouleurCarte.h
@@ -10,3 +10,17 @@ enum class CouleurCarte
     Vert,
     Locomotive
 };
+
+inline std::string couleurToString(CouleurCarte couleur)
+{
+    switch (couleur) {
+        case CouleurCarte::Bleu:       return "Bleu";
+        case CouleurCarte::Blanc:      return "Blanc";
+        case CouleurCarte::Rouge:      return "Rouge";
+        case CouleurCarte::Noir:       return "Noir";
+        case CouleurCarte::Jaune:      return "Jaune";
+        case CouleurCarte::Vert:       return "Vert";
+        case CouleurCarte::Locomotive: return "Locomotive";
+        default:                       return "Inconnu";
+    }
+}
