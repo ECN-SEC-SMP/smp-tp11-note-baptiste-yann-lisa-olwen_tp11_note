ae7c4b5f5063cfbb19d8dd07e4c3acc3dbee61fa
Lisa Le Goff <lisalegoff29@gmail.com>
2026-03-31 22:02:19 +0200

    chore(cmake): remove build on github + add in .gitignore
    ON NE METS PAS LES FICHIERS DE BUILD SUR LE *** DE DISTANT WESH RESPECTEZ VOUS

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_AventuriersDuRail_FILE /Users/olwen/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Cours ECN/SMP/TP11_note/smp-tp11-note-baptiste-yann-lisa-olwen_tp11_note/build/AventuriersDuRail)
-set(__QT_DEPLOY_TARGET_AventuriersDuRail_TYPE EXECUTABLE)
